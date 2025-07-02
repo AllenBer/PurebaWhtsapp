c51a488b25d81b142c2d3d9e8d2503ebc1d5b677
AllenBer <allenbernardo655@gmail.com>
2025-07-02 16:12:11 -0600

    script1.js

--- script1.js
+++ script1.js
@@ -18,7 +18,7 @@ let authInstance;
 const images = {};
 let zipBlob = null;
 
-function compressImage(blob, maxWidth = 640, quality = 0.5) {
+function compressImage(blob, maxWidth = 700, quality = 0.8) {
   return new Promise((resolve) => {
     const img = new Image();
     const url = URL.createObjectURL(blob);
